225ee6505f11d3e6bd8ff13c47e7fe28b1c0d094
Valentina <valenrobledo31@gmail.com>
2019-11-28 16:30:06 -0500

    Add files via upload
    Connecting server with Arduino

--- telemetria2.php
+++ telemetria2.php
@@ -2,22 +2,39 @@
 
 $host='localhost';
 $db = 'telemetria';
-$username = 'telemetria';
-$password = '123456';
+$username = 'root';
+$password = '';
 
 $dsn= "mysql:host=$host;dbname=$db";
- 
+
 try{
- // create a PDO connection with the configuration data
- $conn = new PDO($dsn, $username, $password);
- 
- // display a message if connected to database successfully
- if($conn){
- echo "Connected to the <strong>$db</strong> database successfully!";
-        }
+	 // create a PDO connection with the configuration data
+	 $conn = new PDO($dsn, $username, $password);
+
+	 // display a message if connected to database successfully
+	 if($conn){
+
+	 		$cardIdRC = $_GET["cardID"];
+
+
+	 		$STH = $conn -> prepare( "select id from estudiantes where cardId = ?" );
+			$STH -> execute([$cardIdRC]);
+			$result = $STH -> fetch();
+
+			if ($result["id"]) {
+
+			
+				$sql = "INSERT INTO asistencia (id_estudiante) VALUES (?)";
+				$conn->prepare($sql)->execute([$result["id"]]);
+
+				echo $result["id"];
+
+			}
+	        
+	}
 }catch (PDOException $e){
  // report error message
- echo $e->getMessage();
+ 	echo $e->getMessage();
 }
 
 ?>
